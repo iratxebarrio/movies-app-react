f090abe7dd7d301a6f48c9da388da972fc4a5335
Iratxe <iratxe92@gmail.com>
2024-04-10 16:24:49 +0200

    Fix localStorage information

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import MoviesApp from './MoviesApp.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MoviesApp from './MoviesApp.jsx';
+import './index.css';
 import { I18nextProvider } from "react-i18next";
-import i18next from "i18next"
-import global_es from "../src/translations/es/global.json"
-import global_en from "../src/translations/en/global.json"
-import {Provider} from 'react-redux'
+import i18next from "i18next";
+import global_es from "../src/translations/es/global.json";
+import global_en from "../src/translations/en/global.json";
+import { Provider } from 'react-redux';
 import { store } from './store/store.js';
 
+// Get the initial language of the LocalStorage or set a default one
+const initialLanguage = localStorage.getItem('settings') ? JSON.parse(localStorage.getItem('settings')).language : 'es';
 
+// Initialize i18next with initial language
 i18next.init({
-  interpolation: {escapeValue: false},
-  lng: "es",
+  interpolation: { escapeValue: false },
+  lng: initialLanguage,
   resources: {
     es: {
       global: global_es
@@ -21,14 +24,16 @@ i18next.init({
       global: global_en
     }
   }
-})
+});
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <Provider store={store}>
-     <I18nextProvider i18n={i18next}>
-    <MoviesApp />
-     </I18nextProvider>
-      </Provider>
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <I18nextProvider i18n={i18next}>
+        <MoviesApp  />
+      </I18nextProvider>
+    </Provider>
+  </React.StrictMode>
+);
--- src/store/theme/themeSlice.js
+++ src/store/theme/themeSlice.js
@@ -2,15 +2,20 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const themeSlice = createSlice({
     name: 'theme',
-    initialState: {
-    theme: 'light'
-},
+    initialState: 
+      localStorage.getItem('settings') 
+      ? JSON.parse(localStorage.getItem('settings')) 
+      : {theme: 'light', language: 'es'},
+
     reducers: {
      changeTheme: (state, {payload} ) => {
         state.theme = payload;
      },
+     changeLanguage: (state, {payload} ) => {
+      state.language = payload;
+     }
   }
 });
 
 
-export const { changeTheme } = themeSlice.actions;
+export const { changeTheme, changeLanguage } = themeSlice.actions;
